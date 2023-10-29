5b369d747faa8cf80be1dc22c1d0f445ca7f4022
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2023-10-29 20:02:30 +0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
      <Provider store={store}>
+          <Loader/>
           <HeadConfig/>
           <Navigation/>
           <Socials/>
